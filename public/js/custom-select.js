e5b9c248ebd49135f6e60aa4bb197eb355951fdf
alessandro.infantini <infa1983@gmail.com>
2023-02-28 13:08:06 +0100

    fix label and structure of select

--- public/js/custom-select.js
+++ public/js/custom-select.js
@@ -1,37 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    /*
-    var customSelect = document.querySelector('.js-custom-select');
-    var customOptions = "";
-    var listOptions;
-    for (listOptions = 0; listOptions < customSelect.length; listOptions++) {
-        customOptions = customOptions + "\n" + customSelect.options[listOptions].value;
-    }
-
-    console.log(customOptions);
-    */
-
+    // create list from options
     var customSelect = document.querySelectorAll('.js-custom-select');
-
     customSelect.forEach((element) => {
         let buttonCustomSelect = document.createElement("button");
-        let containerCustomSelect = document.createElement("ul");
-        let containerCustomSelectList = document.createElement("li");
+        let listCustomSelect = document.createElement("ul");
         let customOptions = "";
+        // parameter
+        listCustomSelect.className = "custom-select-options-list";
+        listCustomSelect.role = "listbox";
         buttonCustomSelect.className = "btn custom-select-button";
-        buttonCustomSelect.ariaLabel = "seleziona un'opzione";
-        containerCustomSelect.className = "custom-select-list-options";
+        //buttonCustomSelect.ariaLabel = "seleziona un'opzione";
 
         for (listOptions = 0; listOptions < element.length; listOptions++) {
-            customOptions = customOptions + "\n" + element.options[listOptions].value;
+            customOptions = customOptions + "<li>" + "<a href='#' class='custom-select-options-list--item' role='option'>" + element.options[listOptions].text + "</a>" + "</li>";
         }
-        
 
         element.parentElement.parentElement.append(buttonCustomSelect);
-        element.parentElement.parentElement.append(containerCustomSelect);
+        element.parentElement.parentElement.append(listCustomSelect);
 
-        containerCustomSelect.append(customOptions);
+        listCustomSelect.innerHTML = customOptions;
+        //console.log(customOptions)
+    });
 
+    // hide and show div with options
+    var showHideListButton = document.querySelectorAll(".custom-select-button");
+    showHideListButton.forEach(element => {
+        element.addEventListener("click", () => {
+            element.parentElement.classList.toggle("open");
+        });
+    });
+
+    // create text button from label and options and add action to link
+    var customSelectGroup = document.querySelectorAll('.custom-select-group');
+    customSelectGroup.forEach(element => {
+        let labelSelect = element.querySelectorAll(".custom-select-label");
+        let customButton = element.querySelector(".custom-select-button");
+        let linkList = element.querySelectorAll(".custom-select-options-list--item");
+
+        for (var i = 0; i < labelSelect.length; i++) {
+            var thisLabelSelect = labelSelect[i].innerText;
+            //console.log(thisLsbelSelect);
+            //console.log(customButton);
+            customButton.textContent = thisLabelSelect;
+        }
+
+        for (var i = 0; i < linkList.length; i++) {
+            linkList[i].addEventListener("click", function () {
+                (element.querySelector('.active')) ? element.querySelector('.active').classList.remove('active') : '';
+                this.classList.add('active');
+                this.parentElement.parentElement.parentElement.classList.remove('open');
+                customButton.textContent = element.querySelector('.active').innerText;
+            });
+        }
     });
 
 });
